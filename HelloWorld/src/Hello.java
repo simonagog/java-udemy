71569fb9376028c4b82b1257d19981911d527fc9
simonik92 <caltea.simona@yahoo.com>
2025-10-13 15:54:39 +0300

    creating classes and if statement

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("I'm scared");
+        }
+
+        int score = 100;
+        if ((score > 90) && (score < 110))
+        {
+            System.out.println("Perfect");
+        }
+    }
+}
