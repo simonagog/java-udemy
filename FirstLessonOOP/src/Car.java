1065b1ce7a17099078e008664ba935928f00b155
simonik92 <caltea.simona@yahoo.com>
2026-02-06 15:32:10 +0200

    while Loop

--- FirstLessonOOP/src/Car.java
+++ FirstLessonOOP/src/Car.java
@@ -26,6 +26,27 @@ public class Car {
         return make;
     }
 
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
     public void describeCar()
     {
         System.out.println(doors + "-Doors "
--- FirstLessonOOP/src/Main.java
+++ FirstLessonOOP/src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
         car.describeCar();
+        car.setModel("Porsche");
+        car.setColor("red");
+        car.setDoors(2);
         System.out.println("make: " + car.getMake());
-        System.out.println("mode: " + car.getModel());
+        System.out.println("model: " + car.getModel());
         System.out.println("color: " + car.getColor());
     }
 }
