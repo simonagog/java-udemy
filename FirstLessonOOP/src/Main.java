fdde0f2bd698dee412a57a3950166df195cc171a
simonik92 <caltea.simona@yahoo.com>
2026-02-11 15:50:13 +0200

    Methods

--- FirstLessonOOP/src/Main.java
+++ FirstLessonOOP/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        car.describeCar();
         car.setModel("Porsche");
         car.setColor("red");
         car.setDoors(2);
+
+        car.describeCar();
+
         System.out.println("make: " + car.getMake());
         System.out.println("model: " + car.getModel());
         System.out.println("color: " + car.getColor());
