a27fec9ec03db11c67f81d66b88db99df7758448
simonik92 <caltea.simona@yahoo.com>
2026-02-11 16:35:41 +0200

    depositing and withdrawing

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
         bankAccount.withdrawingFunds(100);
+        bankAccount.depositingFunds(100);
+        bankAccount.withdrawingFunds(50);
+        bankAccount.withdrawingFunds(50);
     }
 }
