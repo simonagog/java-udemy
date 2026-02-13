d98f59457313746e0ca93379539a30544c7c2e99
simonik92 <caltea.simona@yahoo.com>
2026-02-13 15:46:20 +0200

    bankAccount

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         BankAccount bankAccount = new BankAccount();
+        bankAccount.setAccountNumber("123123123123");
+        bankAccount.setAccountBalance(100.0);
+        bankAccount.setCustomerName("Pop Ioan");
+        bankAccount.setPhoneNumber("0723123123");
+        bankAccount.setEmail("popioan@gmail.com");
         bankAccount.withdrawingFunds(100);
         bankAccount.depositingFunds(100);
         bankAccount.withdrawingFunds(50);
