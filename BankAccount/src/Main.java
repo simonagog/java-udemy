42e85fa1e58bb6dc0be224a8d082882f4385052d
simonik92 <caltea.simona@yahoo.com>
2026-02-18 09:38:54 +0200

    constructors

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         BankAccount bankAccount = new BankAccount();
 
-        BankAccount newBankAccount = new BankAccount("123456789", 200.0, "Alexa", "alexa@gmail.com","0745123123");
+        BankAccount newBankAccount = new BankAccount("123456789", 200.0, "Alexa", "alexa@gmail.com", "0745123123");
         bankAccount.setAccountNumber("123123123123");
         bankAccount.setAccountBalance(100.0);
         bankAccount.setCustomerName("Pop Ioan");
@@ -13,5 +13,21 @@ public class Main {
         bankAccount.depositingFunds(100);
         bankAccount.withdrawingFunds(50);
         bankAccount.withdrawingFunds(50);
+
+        Customer customer = new Customer("Simona", 100000, "simona@yahoo.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmailAddress());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmailAddress());
+
+        Customer thirdCustomer = new Customer("Joe", "joe@yahoo.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmailAddress());
+
     }
 }
