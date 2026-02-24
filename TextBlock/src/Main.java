9e88304f61c190321f0d3950d6490402d035bc89
simonik92 <caltea.simona@yahoo.com>
2026-02-24 09:44:48 +0200

    create TextBlock

---
+++ TextBlock/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        String bulletIt = "Print a Bulleted List:\n" + //\n represent new line
+                "\t\u2022 First Point\n" + //\t - tab
+                "\t\t\u2022 Sub Point"; //\u2022 - bullet
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                            \u2022 First Point
+                                \u2022 Sub Point""";
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float)age);
+
+        for (int i = 1; i <= 100000; i *= 10)
+        {
+            System.out.printf("Printing %d %n", i);
+        }
+
+        for (int i = 1; i <= 100000; i *= 10)
+        {
+            System.out.printf("Printing %6d %n", i);
+        }
+
+        String formattedString = String.format("your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+    }
+
+}
